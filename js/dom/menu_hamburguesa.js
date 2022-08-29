98a59db9cc36dc9df7f17c9babfd800cba0f10e2
javier1793-op <javier17unt@gmail.com>
2022-08-28 21:05:49 -0300

    menu_animacion

--- js/dom/menu_hamburguesa.js
+++ js/dom/menu_hamburguesa.js
@@ -1,1 +1,16 @@
 /* **********     Curso JavaScript: 81. DOM: Ejercicios Prácticos | Menú de Hamburguesa - #jonmircha     ********** */
+export default function hamburguesaMenu(panelbtn,panel,menuLink){
+ const d= document
+
+ d.addEventListener("click", (e) =>{
+    if(e.target.matches(panelbtn) || (e.target.matches(`${panelbtn} *`))){
+      d.querySelector(panel).classList.toggle("is-active");  
+      d.querySelector(panelbtn).classList.toggle("is-active");  
+    }
+
+    if(e.target.matches(menuLink)){
+        d.querySelector(panel).classList.remove("is-active");  
+        d.querySelector(panelbtn).classList.remove("is-active");  
+    }
+ });
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,1 +1,10 @@
 /* **********     Curso JavaScript: 80. DOM: Ejercicios Prácticos | Presentación - #jonmircha     ********** */
+
+import hamburguesaMenu from "./dom/menu_hamburguesa.js";
+
+const d= document;
+
+d.addEventListener("DOMContentLoaded", e =>{
+    hamburguesaMenu(".panel-btn", ".panel",".menu a");
+});
+
