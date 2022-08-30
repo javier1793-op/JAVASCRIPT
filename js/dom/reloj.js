3e4bbb19768cac731ee7bfcfc76a88ba1b2031a7
javier1793-op <javier17unt@gmail.com>
2022-08-29 21:39:21 -0300

    reloj

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -1,1 +1,26 @@
 /* **********     Curso JavaScript: 82. DOM: Ejercicios Prácticos | Reloj Digital - #jonmircha     ********** */
+const d= document;
+
+export function digitalClock(clock,btnini,btnstop){
+ 
+let clockTempo;    
+    d.addEventListener("click", (e)=>{
+    if(e.target.matches(btnini)){
+        clockTempo= setInterval(()=>{
+            let clockHour= new Date().toLocaleTimeString();
+            d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;
+        },1000);
+        e.target.disabled=true;
+    }
+    
+    if(e.target.matches(btnstop)){
+        clearInterval(clockTempo);
+        d.querySelector(clock).innerHTML= null;
+        d.querySelector(btnini).disabled=false;
+    }
+ })
+};
+
+export function Alarm(){
+
+};
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,10 +1,12 @@
 /* **********     Curso JavaScript: 80. DOM: Ejercicios Prácticos | Presentación - #jonmircha     ********** */
 
 import hamburguesaMenu from "./dom/menu_hamburguesa.js";
+import {digitalClock,Alarm} from "./dom/reloj.js";
 
 const d= document;
 
 d.addEventListener("DOMContentLoaded", e =>{
     hamburguesaMenu(".panel-btn", ".panel",".menu a");
+    digitalClock("#reloj","#iniciar-reloj","#detener-reloj");
 });
 
